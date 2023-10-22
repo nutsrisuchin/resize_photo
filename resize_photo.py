6c8be3d9b10233a1206c2383694419e6894468cb
Nut Sr T-II-IP2/1977 <26005064@pttgcgroup.com>
2023-10-22 10:00:48 +0700

    added

--- resize_photo.py
+++ resize_photo.py
@@ -3,29 +3,36 @@ from PIL import Image
 import io
 import zipfile
 
-def resize_image(uploaded_image, target_resolution=(1920, 1080)):
+def resize_image(uploaded_image, target_width=1920):
     try:
         img = Image.open(uploaded_image)
-        img_resized = img.resize(target_resolution, Image.ANTIALIAS)
+
+        # Calculate the aspect ratio
+        aspect_ratio = img.height / img.width
+        new_height = int(target_width * aspect_ratio)
+
+        img_resized = img.resize((target_width, new_height), Image.ANTIALIAS)
         return img_resized
     except Exception as e:
         st.error(f"Error processing image: {e}")
         return None
 
-st.title("Image Resizer to 1920x1080")
+st.title("Image Resizer to 1920 Width")
 
 uploaded_images = st.file_uploader("Upload images", type=["jpg", "png", "jpeg"], accept_multiple_files=True)
 
 if uploaded_images:
+    # Counters for uploaded and resized images
+    uploaded_count = len(uploaded_images)
+    resized_count = 0
+    
     # Create an in-memory bytes buffer for the zip file
     zip_buffer = io.BytesIO()
 
     # Create a zip file to store resized images
     with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zf:
         for uploaded_image in uploaded_images:
-            st.write(f"**Processing: {uploaded_image.name}**")
             try:
-                st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
                 resized_img = resize_image(uploaded_image)
 
                 if resized_img:
@@ -35,10 +42,14 @@ if uploaded_images:
                     img_bytes = img_buffer.getvalue()
                     zf.writestr(f"resized_{uploaded_image.name}", img_bytes)
                     
-                    st.image(resized_img, caption="Resized Image.", use_column_width=True)
+                    # Increment the counter for successfully resized images
+                    resized_count += 1
 
             except Exception as e:
                 st.error(f"Error processing image: {e}")
+
+    st.write(f"**Number of uploaded images:** {uploaded_count}")
+    st.write(f"**Number of successfully resized images:** {resized_count}")
 
     # Provide a download button for the zip file
     st.write("Download all resized images in a ZIP file:")
@@ -49,6 +60,4 @@ if uploaded_images:
         file_name="resized_images.zip",
         mime="application/zip"
     )
-
-
 
