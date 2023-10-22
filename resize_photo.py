53ca763b277b7f68a16f2ab9f195aa5acad0e6c2
Nut Sr T-II-IP2/1977 <26005064@pttgcgroup.com>
2023-10-22 09:51:11 +0700

    revised

--- resize_photo.py
+++ resize_photo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import io
+import zipfile
 
 def resize_image(uploaded_image, target_resolution=(1920, 1080)):
     try:
@@ -16,25 +17,38 @@ st.title("Image Resizer to 1920x1080")
 uploaded_images = st.file_uploader("Upload images", type=["jpg", "png", "jpeg"], accept_multiple_files=True)
 
 if uploaded_images:
-    for uploaded_image in uploaded_images:
-        st.write(f"**Processing: {uploaded_image.name}**")
-        try:
-            st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
-            resized_img = resize_image(uploaded_image)
-            
-            if resized_img:
-                img_buffer = io.BytesIO()
-                resized_img.save(img_buffer, format="JPEG")
-                img_bytes = img_buffer.getvalue()
-                
-                st.image(resized_img, caption="Resized Image.", use_column_width=True)
-                st.download_button(
-                    label="Download Resized Image",
-                    data=img_bytes,
-                    file_name=f"resized_{uploaded_image.name}",
-                    mime="image/jpeg"
-                )
-        except Exception as e:
-            st.error(f"Error displaying or downloading image: {e}")
+    # Create an in-memory bytes buffer for the zip file
+    zip_buffer = io.BytesIO()
+
+    # Create a zip file to store resized images
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zf:
+        for uploaded_image in uploaded_images:
+            st.write(f"**Processing: {uploaded_image.name}**")
+            try:
+                st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
+                resized_img = resize_image(uploaded_image)
+
+                if resized_img:
+                    # Save the resized image into the zip file
+                    img_buffer = io.BytesIO()
+                    resized_img.save(img_buffer, format="JPEG")
+                    img_bytes = img_buffer.getvalue()
+                    zf.writestr(f"resized_{uploaded_image.name}", img_bytes)
+                    
+                    st.image(resized_img, caption="Resized Image.", use_column_width=True)
+
+            except Exception as e:
+                st.error(f"Error processing image: {e}")
+
+    # Provide a download button for the zip file
+    st.write("Download all resized images in a ZIP file:")
+    zip_buffer.seek(0)  # Reset buffer position
+    st.download_button(
+        label="Download ZIP",
+        data=zip_buffer.getvalue(),
+        file_name="resized_images.zip",
+        mime="application/zip"
+    )
+
 
 
