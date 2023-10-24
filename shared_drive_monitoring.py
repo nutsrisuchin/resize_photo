65761f5550d2ccbfc8c19fcce527e1b5d6cbcdec
Nut Sr T-II-IP2/1977 <26005064@pttgcgroup.com>
2023-10-24 15:12:28 +0700

    added

--- shared_drive_monitoring.py
+++ shared_drive_monitoring.py
@@ -1,59 +1,50 @@
-import os
-import json
 import streamlit as st
+import json
 import pandas as pd
+import os
 
-# Conversion Functions
-def bytes_to_mb(size_in_bytes):
-    return size_in_bytes / (2**20)  # Convert bytes to megabytes
+BYTES_IN_GB = 1e9  # 1 GB = 1e9 bytes
 
-# Visualization Functions
-def display_folder_data(folders, level=1):
-    # Ensure sizes are converted to MB for display
-    for folder in folders:
-        folder_size = bytes_to_mb(folder["size"])
-        st.write(f"{'--' * level} {folder['name']} - {folder_size:.2f} MB")
-        if "subfolders" in folder:
-            display_folder_data(folder["subfolders"], level + 1)
-
-def visualize_drive_space(drive, drive_info):
-    # Convert drive info sizes to MB for display
-    total_mb = bytes_to_mb(drive_info["total"])
-    used_mb = bytes_to_mb(drive_info["used"])
-    free_mb = bytes_to_mb(drive_info["free"])
-    
-    st.write(f"Total space on {drive}: {total_mb:.2f} MB")
-    st.write(f"Used space on {drive}: {used_mb:.2f} MB")
-    st.write(f"Free space on {drive}: {free_mb:.2f} MB")
-
-def visualize_file_type_sizes(file_types):
-    file_type_data = {f_type: bytes_to_mb(f_size) for f_type, f_size in file_types.items()}
-    file_type_df = pd.DataFrame(list(file_type_data.items()), columns=['File Type', 'Size (MB)'])
-    st.table(file_type_df.sort_values('Size (MB)', ascending=False))
-
-# Load extracted data from JSON
+# Load data from JSON
 with open("drive_data.json", "r") as f:
     data = json.load(f)
 
-# Streamlit Visualization
-st.title("Shared Drive Monitoring")
+# Streamlit app
+def app():
+    # Allow user to select the drive/folder
+    selected_drive = st.selectbox("Select drive/folder", list(data.keys()))
+    drive_data = data[selected_drive]
+    
+    st.title(f"Data for {selected_drive}")
+    
+    # Display drive info
+    st.subheader("Drive Info")
+    st.write(f"Total space: {drive_data['drive_info']['total'] / BYTES_IN_GB:.2f} GB")
+    st.write(f"Used space: {drive_data['drive_info']['used'] / BYTES_IN_GB:.2f} GB")
+    st.write(f"Free space: {drive_data['drive_info']['free'] / BYTES_IN_GB:.2f} GB")
 
-selected_drive = st.selectbox("Select a drive to monitor", list(data.keys()))
-drive_data = data[selected_drive]
+    # Allow user to specify subfolder to view
+    subfolders = [sf["name"] for sf in drive_data["subfolders"]]
+    selected_subfolder = st.selectbox("Select subfolder", ["-"] + subfolders)
+    
+    if selected_subfolder != "-":
+        for sf in drive_data["subfolders"]:
+            if sf["name"] == selected_subfolder:
+                st.subheader(f"Data for {selected_subfolder}")
+                st.write(f"Used space: {sf['size'] / BYTES_IN_GB:.2f} GB")
+                # Table for file type sizes
+                st.subheader("Space by File Type (in GB)")
+                file_type_df = pd.DataFrame(sf["file_types"].items(), columns=["File Type", "Size"])
+                file_type_df["Size"] = file_type_df["Size"] / BYTES_IN_GB
+                st.table(file_type_df)
+                break
+    
+    # Pie chart for space used by 1-level subfolders
+    st.subheader("Space Distribution of 1-level Subfolders")
+    folder_names = [sf["name"] for sf in drive_data["subfolders"]]
+    folder_sizes = [sf["size"] for sf in drive_data["subfolders"]]
+    st.pie_chart(pd.Series(folder_sizes, index=folder_names))
 
-visualize_drive_space(selected_drive, drive_data["drive_info"])
+if __name__ == "__main__":
+    app()
 
-st.write("## File Type Statistics at Top Level")
-visualize_file_type_sizes(drive_data["file_types"])
-
-st.write("## Subfolder Data")
-display_folder_data(drive_data["subfolders"])
-
-# Extract file type data for subfolders
-all_file_types = {}
-for folder in drive_data["subfolders"]:
-    for f_type, f_size in folder["file_types"].items():
-        all_file_types[f_type] = all_file_types.get(f_type, 0) + f_size
-
-st.write("## File Type Statistics for Subfolders")
-visualize_file_type_sizes(all_file_types)
