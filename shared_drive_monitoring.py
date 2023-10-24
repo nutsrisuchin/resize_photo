d0f49191978ccea7d8ae5f703636a2779a15d06a
Nut Sr T-II-IP2/1977 <26005064@pttgcgroup.com>
2023-10-24 10:58:25 +0700

    added

--- shared_drive_monitoring.py
+++ shared_drive_monitoring.py
@@ -4,22 +4,32 @@ import os
 import psutil
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # ... [Previous utility functions]
 
-# Additional function to visualize drive space
-def visualize_drive_space(drive, drive_info):
-    df = pd.DataFrame({
-        'Space': ['Used', 'Free'],
-        'GB': [drive_info['used'] / (1024**3), drive_info['free'] / (1024**3)]
-    })
-    st.bar_chart(df.set_index('Space'))
+# Visualization function for subfolder comparison
+def visualize_subfolder_comparison(subfolders):
+    folder_sizes = [f['size'] for f in subfolders]
+    folder_names = [f['name'] for f in subfolders]
 
-# ... [display_folder_data function]
+    df = pd.DataFrame({
+        'Folder': folder_names,
+        'Size (GB)': [size / (1024**3) for size in folder_sizes]
+    })
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    df.sort_values('Size (GB)', ascending=True).plot(kind='barh', x='Folder', y='Size (GB)', ax=ax)
+    plt.title('Subfolder Size Comparison')
+    plt.tight_layout()
+    st.pyplot(fig)
 
 def app():
     st.title("Shared Drive Space Monitoring")
     
+    # Sidebar option to show subfolders
+    show_subfolders = st.sidebar.checkbox("Show Subfolder Details", True)
+
     # Load data from JSON file
     with open("drive_data.json", "r") as f:
         data = json.load(f)
@@ -36,23 +46,17 @@ def app():
         st.write(f"Free Space: {drive_info['free'] / (1024**3):.2f} GB")
         st.write(f"Percent Used: {drive_info['percent_used']}%")
         
-        # Pie chart for top-level folder distribution
-        folder_sizes = [f['size'] for f in drive_data['subfolders']]
-        folder_names = [f['name'] for f in drive_data['subfolders']]
-        st.pie_chart(pd.Series(folder_sizes, index=folder_names))
+        # Visualization for subfolder comparison
+        visualize_subfolder_comparison(drive_data["subfolders"])
         
-        st.write("\n")
-        st.write("### Subfolder Sizes")
-        display_folder_data(drive_data["subfolders"])
-
-    # Assuming you have historical data stored in a CSV
-    if os.path.exists("historical_data.csv"):
-        df = pd.read_csv("historical_data.csv")
-        st.write("### Drive Usage Trend")
-        st.line_chart(df.set_index('Date'))
+        if show_subfolders:
+            st.write("\n")
+            st.write("### Subfolder Sizes")
+            display_folder_data(drive_data["subfolders"])
 
 # ... [App Execution]
 
 if __name__ == "__main__":
     app()
+
 
